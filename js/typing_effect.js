bd5ea901195eb43ce6b00cd2caa35fe732d14a4f
Haneul Lee <haneul.lee@digipen.edu>
2022-08-01 22:51:52 +0900

    Add files via upload

--- js/typing_effect.js
+++ js/typing_effect.js
@@ -1,31 +1,23 @@
 const $typing_effect = document.querySelector(".typing_effect");
 
-console.log($typing_effect);
-
-// 글자 모음
 const letters = ["C++", "C", "C#", "HTML", "CSS", "JavaScript"];
 
-// 글자 입력 속도
 const speed = 100;
 let i = 0;
 
-// 타이핑 효과
 const typing = async () => {  
   const letter = letters[i].split("");
-  
+
   while (letter.length) {
     await wait(speed);
     $typing_effect.innerHTML += letter.shift(); 
   }
   
-  // 잠시 대기
   await wait(800);
   
-  // 지우는 효과
-  if (letters[i + 1]) remove();
+  if (letters[i]) remove();
 }
 
-// 글자 지우는 효과
 const remove = async () => {
   const letter = letters[i].split("");
   
@@ -36,8 +28,10 @@ const remove = async () => {
     $typing_effect.innerHTML = letter.join(""); 
   }
   
-  // 다음 순서의 글자로 지정, 타이핑 함수 다시 실행
   i++;
+  if(i > letters.length - 1)
+    i = 0;
+
   typing();
 }
 
@@ -47,4 +41,4 @@ function wait(ms) {
 }
 
 // 초기 실행
-setTimeout(typing, 2700);
+setTimeout(typing, 1500);
